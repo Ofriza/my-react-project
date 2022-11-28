c90634968d06e5447471f1a2acd05fd97240d595
Ofriza <109228180+Ofriza@users.noreply.github.com>
2022-11-28 22:53:13 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import react from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import axios from 'axios'
+
+
+const reactContentRoot = document.getElementById("root")
+
+
+ReactDOM.render(<App />, reactContentRoot)
